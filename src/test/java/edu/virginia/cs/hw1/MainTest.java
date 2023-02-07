9ae1699fdbc2cb1ba3e67b5099c3b3d31e121b74
ChrisxCross2003 <tbh7cm@virginia.edu>
2023-02-07 17:01:36 -0500

    Did some testing, compiled and gave expected values. Deleted a JUnit test file as it was no longer needed.

--- src/test/java/edu/virginia/cs/hw1/MainTest.java
+++ src/test/java/edu/virginia/cs/hw1/MainTest.java
@@ -1,5 +1,7 @@
 package edu.virginia.cs.hw1;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +9,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
+    @DisplayName("Scenario 1: ")
     void main() {
+        // 1. Test input
+
+        // 2. Expected Output
+        int expected = 0;
+        // 3. Actual Output
+        //int actual = hw1_test_code.main([arguments]);
+        // 4. Compare Expected and Actual
+        //assertEquals(expected, actual);
     }
 }
